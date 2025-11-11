93a6f0cc3662ad0b386410e942a87837c0cb3e75
habiba16001804 <habiba.hammad@student.giu-uni.de>
2025-11-11 09:54:56 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,10 +1,27 @@
 #ifndef COURSE_H
 #define COURSE_H
-
+#include "Course.h"
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+//#wrlass Course{
+    private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    public:
+    addStudent(const Student& s){
+    Student* students++;
+    }
+    displayCourseInfo(){
+        cout<<"course:"<<name<<"year:"<<yearLevel<<"major:"<<"department:"<<department<<"experience years:"<< experienceYears<<endl;
+        cout<<"code:"<<courseCode<<"course name:"<<courseName<<"max students:"<<maxStudents<<"current:"<<currentStudents<<endl;
+    };
+
+}
+#endif 
 
 
 
