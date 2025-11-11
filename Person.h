f4e4688c13450e3c8cd0769087c282a3f90847ac
habiba16001804 <habiba.hammad@student.giu-uni.de>
2025-11-11 09:58:00 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -3,14 +3,43 @@
 
 #include <string>
 using namespace std;
+class Person{
+private:
+    string name;
+    int id;
+public:
+display();
+Person();
+Person(string name, int id);
 
-//#write Person class here
+Person(string n, int i){
+   name=n;
+   id=i;
+}
+Person:: void display(){
+    cout<<"name:"<<name;<<"id:"<<id<<endl;
+}
+string Person:: getName(){
+    return name;
+}
+string person:: setName(){
+    name="";
+}
+int Person::getID(){
+    return id;
+}
+int Person:: setID(){
+    id=0;
+}
+};
+#endif
 
 
 
 
 
 
+//completed class person
 
 
 
