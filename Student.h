8de0c484101e7227f9a504acf267b48b15a98835
habiba16001804 <habiba.hammad@student.giu-uni.de>
2025-11-11 09:59:33 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -4,7 +4,38 @@
 #include "Person.h"
 #include <string>
 #using namespace std;
-//#write student class here
+class Student{
+    private:
+    int yearLevel;
+    string major;
+    public:
+    Student(){
+        yearLevel=0;
+        major="";
+    }
+    display();
+    Student:: void display(){
+        cout<<"year:"<<yearLevel;<<"major:"<<major<<endl;
+    }
+    Student:: public Person(int yearLevel,string major){
+        this.yearLevel=yearLevel;
+        this.major=major;
+    }
+    String Student::getMajor(){
+        return major;
+    }
+    String Student:: setMajor(){
+        major="";
+    }
+    int Student:: getyearLevel(){
+        return yearLevel;
+    }
+    int student:: setyearLevel(){
+        yearLevel=0;
+    }
+
+};
+
 
 
 
